7f8e021af0623bc0de4ca7a02de6c99e9bbe62e3
hessam <hbagherinezhad@gmail.com>
2013-12-14 11:46:31 +0330

    Register Compnay Done.

--- static/js/base.js
+++ static/js/base.js
@@ -3,4 +3,5 @@ $(document).ready(relaxUI);
 function relaxUI(){
     $('.ui.checkbox').checkbox();
     $('.ui.radio.checkbox').checkbox();
+    $('.ui.dropdown').dropdown();
 }
---
+++ static/js/page.js
@@ -0,0 +1,5 @@
+$(document).ready( function(){
+   $("#searchJob").change(function(){
+      window.location.href = "/search";
+   });
+});
