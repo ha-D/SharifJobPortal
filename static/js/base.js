336da07de1471729a0eede6a3b525c7f7ce82ed2
ha-D <hadzolfaghari@gmail.com>
2013-12-14 11:47:21 +0330

    Changes

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,9 @@
-$(document).ready(relaxUI);
-
 function relaxUI(){
     $('.ui.checkbox').checkbox();
     $('.ui.radio.checkbox').checkbox();
 }
+
+$(document).ready(function(){
+	relaxUI();
+	$(".footer").load('/footer');
+});
