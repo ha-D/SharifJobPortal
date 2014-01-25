f58b037c7ba4693bf86c625043a73cc8464b3905
unknown <nasihati.s@gmail.com>
2014-01-25 09:18:24 +0330

    few changes

--- static/js/userpanel.js
+++ static/js/userpanel.js
@@ -123,6 +123,7 @@ $(function(){
 
     $("#sendMail").click(function(){sendEmail()});
 
+    $("#userpanel #friends_search .search").click(function(){searchFriends();});
 })
 
 function sendEmail(){
@@ -153,8 +154,81 @@ function sendEmail(){
         },
         error: function(){
             console.log('gand khord');
-
+        },
+        complete: function(){
+            location = "/userpanel/inbox/list/";
         }
     });
+
+}
+
+function sendFriendShipRequest(invited_user){
+    console.log('invited user is ' + invited_user)
+
+    var ajaxData = {
+        "invitedUser" : invited_user
+
+    };
+    $.ajax({
+        url : "requestFriendShip",
+        type : 'post',
+        dataType : 'json',
+        data : ajaxData,
+        success : function(data, status, xhr) {
+            if (data.result == 0) {
+                // Request error
+                console.log("error");
+            } else {
+                alert(data.status)
+            }
+        },
+        error: function(){
+            console.log('gand khord');
+            alert('ajax gand khord');
+        }
+    });
+}
+
+function searchFriends(){
+    $("#friends_search").children().slice(1).remove();
+
+    var ajaxData = {
+        "name" : $("#friends_search input").val()
+    };
+    $.ajax({
+        url : "search",
+        type : 'post',
+        dataType : 'json',
+        data : ajaxData,
+        success : function(data, status, xhr) {
+            if (data.result == 0) {
+                // Request error
+                console.log("error");
+            } else {
+                console.log("liste adama daryaft shod");
+                friends = data.friends;
+                console.log(friends);
+                for(var i in friends){
+                    $("<div class = 'ui segment borderless'></div>").appendTo($("#friends_search"));
+                    $("<img class='rounded ui image' src=" + friends[i].pic + ">").appendTo($("#friends_search .segment").last());
+                    $("<div class='nametag'>" + friends[i].name + "</div>").appendTo($("#friends_search .segment").last());
+                    if ( friends[i].isFriend == false){
+                        $("<div _id=" + friends[i].id + " class='mini red circular ui button'>دعوت به دوستی</div>").appendTo($("#friends_search .segment").last()).click(
+                            function(event){
+                                sendFriendShipRequest(event.target.getAttribute('_id'));
+                            });
+                    }
+                }
+                if (friends.length ==0){
+                    $("<div class='ui segment borderless'> هیچ نتیجه ای پیدا نشد!</div>").appendTo($("#friends_search"));
+                }
+            }
+        },
+        error: function(){
+            console.log('gand khord');
+            alert('ajax gand khord');
+        }
+    });
+
 
 }
