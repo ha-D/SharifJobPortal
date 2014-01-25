25399ccc47847a57d64c0f587bd3c05fac55d6c8
hessam <hbagherinezhad@gmail.com>
2014-01-25 07:54:47 +0330

    New and edit jobs

--- src/jobs/urls.py
+++ src/jobs/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-from jobs.views import applyJob, refuseJob, acceptOffer, rejectOffer, deleteJob, editJob
+from jobs.views import applyJob, refuseJob, acceptOffer, rejectOffer, deleteJob, editJob, newJob, jobPage
 
 urlpatterns = patterns('',
     url(r'apply/(?P<jobid>\d+)$', applyJob),
     url(r'^refuse/(?P<jobid>\d+)$', refuseJob),
     url(r'^delete/(?P<jobid>\d+)$', deleteJob),
-    url(r'^edit/(?P<jobid>\d+)$', editJob),
+    url(r'^edit/(?P<jobid>\d+)/$', editJob),
+    url(r'^new/$', newJob),
     url(r'^offers/accept/(?P<offerid>\d+)$', acceptOffer),
     url(r'^offers/reject/(?P<offerid>\d+)$', rejectOffer),
+    url(r'^(?P<jobid>\d+)/$', jobPage),
 )
